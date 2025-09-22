ee46822722bab496cba4fb1a194264af206f49b3
z.mark <2006zmark@gmail.com>
2025-09-22 10:46:03 +0200

    HelloMekkElek

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,9 +2,14 @@ function Hello(){
 const sayHello = () => {
     window.alert('Helló Világ')
 }
+const sayHelloVki = (nev) =>{
+    window.alert("Helló " +nev)
+}
+var vki = "Mekk Elek";
     return(
         <>
             <button onClick={sayHello}>Helló</button>
+            <button onClick={()=>{sayHelloVki("Mekk Elek")}}>Hello ismerős</button>
         </>
     )
 }
